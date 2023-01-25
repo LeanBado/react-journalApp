d8f929ab4323f0626653eed82dd4b3530be40093
LeanBado <leandro.bado.42@gmail.com>
2023-01-24 22:54:27 -0300

    manteniendo el estado de la autenticación del usuario - OK

--- journal-app/src/router/AppRouter.jsx
+++ journal-app/src/router/AppRouter.jsx
@@ -1,35 +1,46 @@
 import { onAuthStateChanged } from "firebase/auth"
 import { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { Route, Routes } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { FirebaseAuth } from "../firebase/config"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { login, logOut } from "../store/auth/authSlice"
 import { CheckingAuth } from "../ui/components/CheckingAuth"
 
 export const AppRouter = () => {
 
 const {status} = useSelector(state=> state.auth)
+const dispatch = useDispatch()
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async(user)=>{
+      if(!user) return dispatch(logOut())
+
+      const {uid, email, displayName, photoURL} = user
+      
+      dispatch(login({uid, email, displayName, photoURL}))
+
+    })
+  }, []);
+
 
   if(status == "checking"){
     return <CheckingAuth></CheckingAuth>
   }
 
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async(user)=>{
-      console.log
-    })
-  }, []);
-
   return (
     <Routes>
-
-        {/* login y registro*/}
+        {(status == "authenticated")
+        ?// login y registro
         <Route path="/auth/*" element={<AuthRoutes></AuthRoutes>}></Route>
-
-        {/* journalApp */}
+        : //journalApp
         <Route path="/*" element={<JournalRoutes></JournalRoutes>}></Route>
+        }
+        <Route path="/*" element={ <Navigate to="auth/login"></Navigate>}></Route>
+        
 
+        
     </Routes>
   )
 }
